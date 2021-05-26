8528021e0f7fe340430643bcc28e435f5efc114c
Francaolisa <francaolisa@gmail.com>
2021-05-26 16:02:14 +0200

    added settings and gitignore

--- fullwebpage/fullwebpage/settings.py
+++ fullwebpage/fullwebpage/settings.py
@@ -80,10 +80,23 @@ WSGI_APPLICATION = 'fullwebpage.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'francisca4',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
